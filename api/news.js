92007ce556c433c896a540ed4fbf6ef11f6090f0
Rishabh8869 <rishabhsingh202020@gmail.com>
2025-09-07 23:36:10 +0530

    Update api/news.js

--- api/news.js
+++ api/news.js
@@ -1,11 +1,1 @@
-export default async function handler(req, res) {
-  const { country = "us", category = "general" } = req.query;
-  const apiKey = process.env.NEWS_API_KEY;
-  const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&pageSize=100`;
 
-  const response = await fetch(url, {
-    headers: { 'User-Agent': 'Mozilla/5.0' }
-  });
-  const data = await response.json();
-  res.status(200).json(data);
-}
